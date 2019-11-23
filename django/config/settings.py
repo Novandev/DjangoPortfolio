991b9d7f61883b6e700484408fe75f1dc14f8c65
novandev <novanadams35@gmail.com>
2019-11-23 09:27:39 -0800

    added more to the repo

--- django/config/settings.py
+++ django/config/settings.py
@@ -132,4 +132,6 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static")
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/usr/src/app/static'
+
+
